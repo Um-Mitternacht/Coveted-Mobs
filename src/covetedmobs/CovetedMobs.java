9bc0f83a861b04d0bc3e5e3d9864e93ac82c1754
Sunconure11 <eyespot7b@aol.com>
2019-09-24 02:38:20 -0400

    Beginning on the logger

---
+++ src/covetedmobs/CovetedMobs.java
@@ -0,0 +1,16 @@
+public class CovetedMobs {
+	
+	public static final Logger logger = LogManager.getLogger(NAME);
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger.info("We need another and a wiser and perhaps a more mystical concept of animals.");
+		logger.info("Remote from universal nature, and living by complicated artifice, man in civilization surveys the creature through the glass of his knowledge and sees thereby a feather magnified and the whole image in distortion.");
+		logger.info("We patronize them for their incompleteness, for their tragic fate of having taken form so far below ourselves.");
+		logger.info("And therein we err, and greatly err.");
+		logger.info("For the animal shall not be measured by man.");
+		logger.info("In a world older and more complete than ours they moved finished and complete, gifted with extensions of the senses we have lost or never attained, living by voices we shall never hear.");
+		logger.info("They are not brethren, they are not underlings; they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.");
+	}
+	
+}
