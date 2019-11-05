734008f5cdffadfafde7c06a76ac05d0fe0f8991
Sunconure11 <eyespot7b@aol.com>
2019-11-05 01:28:00 -0500

    Blindworm part 1

---
+++ src/covetedmobs/main/java/com/covetedmobs/common/entity/living/herps/EntityBlindworm.java
@@ -0,0 +1,14 @@
+package com.covetedmobs.common.entity.living.herps;
+
+import com.covetedmobs.common.entity.util.ModEntityAnimal;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+/**
+ * Created by Joseph on 11/5/2019.
+ */
+public class EntityBlindworm extends ModEntityAnimal {
+	protected EntityBlindworm(World world, ResourceLocation lootTableLocation) {
+		super(world, lootTableLocation);
+	}
+}
