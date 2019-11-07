9a250ec5db62135896cc632eb88f21934c6e5543
Sunconure11 <eyespot7b@aol.com>
2019-11-07 17:44:35 -0500

    test

--- src/covetedmobs/main/java/com/covetedmobs/registry/ModTextures.java
+++ src/covetedmobs/main/java/com/covetedmobs/registry/ModTextures.java
@@ -1,6 +1,15 @@
 package com.covetedmobs.registry;
 
+import com.covetedmobs.CovetedMobs;
+import net.minecraft.util.ResourceLocation;
+
 /**
  * Created by Joseph on 11/7/2019.
  */
-public class ModTextures {}
+public class ModTextures {
+	
+	private static final String entitytex = CovetedMobs.MODID + ":textures/entities/";
+	
+	// Blindworm
+	public static final ResourceLocation blindworm = new ResourceLocation(entitytex + "blindworm.png");
+}
