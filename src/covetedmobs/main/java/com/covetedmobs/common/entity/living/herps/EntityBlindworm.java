6421ee89e54a97945dbebf2762f53311cec88dfd
Sunconure11 <eyespot7b@aol.com>
2019-11-19 16:55:53 -0500

    Turned down blindworm speed

--- src/covetedmobs/main/java/com/covetedmobs/common/entity/living/herps/EntityBlindworm.java
+++ src/covetedmobs/main/java/com/covetedmobs/common/entity/living/herps/EntityBlindworm.java
@@ -33,11 +33,11 @@ public class EntityBlindworm extends ModEntityAnimal {
 	protected void initEntityAI() {
 		tasks.addTask(0, new EntityAIPanic(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
 		tasks.addTask(0, new EntityAISwimming(this));
-		tasks.addTask(1, new EntityAIMate(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue() / 2));
+		tasks.addTask(1, new EntityAIMate(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
 		tasks.addTask(2, new EntityAIWatchClosest2(this, EntityPlayer.class, 5, 1));
 		tasks.addTask(2, new EntityAIFollowParent(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
 		tasks.addTask(3, new EntityAILookIdle(this));
-		tasks.addTask(4, new EntityAIWander(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue() * 2 / 3));
+		tasks.addTask(3, new EntityAIWander(this, getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
 		this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 0.5D));
 	}
 	
@@ -46,7 +46,7 @@ public class EntityBlindworm extends ModEntityAnimal {
 		super.applyEntityAttributes();
 		getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(6);
 		getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10);
-		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.6);
+		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2);
 	}
 	
 	@Override
