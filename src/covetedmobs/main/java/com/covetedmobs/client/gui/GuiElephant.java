7aea79fd1126dfe406b561a4f48a33da236f3e30
Sunconure11 <eyespot7b@aol.com>
2019-12-16 01:38:17 -0500

    Create GuiElephant.java

---
+++ src/covetedmobs/main/java/com/covetedmobs/client/gui/GuiElephant.java
@@ -0,0 +1,18 @@
+package com.covetedmobs.client.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+/**
+ * Created by Joseph on 12/16/2019.
+ */
+public class GuiElephant extends GuiContainer {
+	public GuiElephant(Container inventorySlotsIn) {
+		super(inventorySlotsIn);
+	}
+	
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+	
+	}
+}
