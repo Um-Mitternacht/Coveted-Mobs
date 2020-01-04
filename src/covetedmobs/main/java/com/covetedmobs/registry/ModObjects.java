93f60571538e2cf48e2500f19dc423c5537f84b5
Sunconure11 <eyespot7b@aol.com>
2020-01-03 20:20:11 -0500

    Start on the newt and blindworm drops

--- src/covetedmobs/main/java/com/covetedmobs/registry/ModObjects.java
+++ src/covetedmobs/main/java/com/covetedmobs/registry/ModObjects.java
@@ -8,11 +8,16 @@ import net.minecraft.item.Item;
  */
 public class ModObjects {
 	
+	//Crafted
 	public static final Item diamond_tusk_sword = Util.registerItem("diamond_tusk_sword");
 	public static final Item iron_tusk_sword = Util.registerItem("iron_tusk_sword");
 	public static final Item gold_tusk_sword = Util.registerItem("gold_tusk_sword");
 	public static final Item howdah = Util.registerItem("howdah");
 	public static final Item garment = Util.registerItem("garment");
+	
+	//Drops
+	public static final Item eye_of_newt = Util.registerItem("eye_of_newt");
+	public static final Item blindworms_sting = Util.registerItem("blindworms_sting");
 	
 	public static void preInit() {
 	}
