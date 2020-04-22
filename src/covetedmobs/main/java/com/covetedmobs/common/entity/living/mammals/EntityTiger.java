bc3bac4f7ceb08619b8341ee0c756246976cf729
Sunconure11 <eyespot7b@aol.com>
2020-04-22 03:45:28 -0400

    Tigers are hostile now

--- src/covetedmobs/main/java/com/covetedmobs/common/entity/living/mammals/EntityTiger.java
+++ src/covetedmobs/main/java/com/covetedmobs/common/entity/living/mammals/EntityTiger.java
@@ -2,7 +2,11 @@ package com.covetedmobs.common.entity.living.mammals;
 
 import com.covetedmobs.CovetedMobs;
 import com.covetedmobs.common.entity.util.ModEntityAnimal;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
@@ -28,6 +32,14 @@ public class EntityTiger extends ModEntityAnimal {
 	@Override
 	protected void initEntityAI() {
 		super.initEntityAI();
+		this.tasks.addTask(1, new EntityAISwimming(this));
+		this.tasks.addTask(3, new EntityAILeapAtTarget(this, 0.4F));
+		this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 0.8D));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 16.0F));
+		this.tasks.addTask(6, new EntityAILookIdle(this));
+		this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, false, (Class<?>) null));
+		this.tasks.addTask(1, new EntityAIAttackMelee(this, getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue(), false));
+		targetTasks.addTask(1, new EntityAINearestAttackableTarget<>(this, EntityLivingBase.class, 10, false, false, e -> (e instanceof EntityPlayer || e instanceof EntitySheep || e instanceof EntityCow || e instanceof EntityChicken || e instanceof EntityLlama || e instanceof EntityPig || e instanceof EntityRabbit || e instanceof AbstractHorse)));
 	}
 	
 	@Override
